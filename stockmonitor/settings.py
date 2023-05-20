1e647e6385b3e008da1e987754ad80293912db84
sahil <souravjangar91@gmail.com>
2023-05-20 11:53:15 +0530

    added changes for static files

--- stockmonitor/settings.py
+++ stockmonitor/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/sahiljoster32/stock_monitor_backend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
